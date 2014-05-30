7caf0bd836da1b4a004b4a3d1c392ba093987259
kdeenkhoorn <kdeenkhoorn@users.noreply.github.com>
2014-05-30 09:20:34 +0200

    Added simpel kernelbuild script

--- buildscript/buildscipt.sh
+++ buildscript/buildscipt.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
+if [ `uname -m` != "armv7l" ]
+then
+	echo "No armv7l hardware found, using CROSS_COMPILE."
+	COMPILEOPTIONS="CROSS_COMPILE=arm-linux-gnueabihf-"
+else
+	echo "armv7l hardware found, not using CROSS_COMPILE."
+	COMPILEOPTIONS=¨
+fi
+
+
 cd ./linux-sunxi
 
 echo "Starting build please check config first ..."
 sleep 10
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+make ARCH=arm ${COMPILEOPTIONS} menuconfig
 
 echo -en "Calculating amount of CPU's available ... "
 CPUS=$(grep '^processor' /proc/cpuinfo | wc -l)
 echo "${CPUS} found."
 
 echo "Starting build of uImage and modules ..."
-make -j${CPUS} ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- uImage modules
-
+make -j${CPUS} ARCH=arm ${COMPILEOPTIONS} uImage modules
 
 echo "Placing compiled modules, firmware and headers in output directory."
-
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=output INSTALL_HDR_PATH=output modules_install headers_install
-
+make ARCH=arm ${COMPILEOPTIONS} INSTALL_MOD_PATH=output INSTALL_HDR_PATH=output modules_install headers_install
 
 echo "Compiling done."
